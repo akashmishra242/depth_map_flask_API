b4ae2876995e3bc9358d152710d3befe73127157
Akash Mishra <akashmishra242@gmail.com>
2023-03-17 23:53:54 +0530

    API added.

---
+++ app.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+from flask import Flask, request, make_response
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the Depth Map API!'
+
+@app.route('/depth-map', methods=['POST'])
+def create_depth_map():
+    # Retrieve the two image files from the request
+    image1 = request.files['image1']
+    image2 = request.files['image2']
+    
+    # Convert the images to grayscale
+    img1 = cv2.imdecode(np.fromstring(image1.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imdecode(np.fromstring(image2.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+
+    # Compute the depth map using OpenCV's StereoBM
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    depth = stereo.compute(img1, img2)
+
+    # Convert the depth map to a PNG image
+    depth_png = cv2.imencode('.png', depth)[1]
+
+    # Return the depth map as a response
+    response = make_response(depth_png.tobytes())
+    response.headers.set('Content-Type', 'image/png')
+    response.headers.set('Content-Disposition', 'attachment', filename='depth_map.png')
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
